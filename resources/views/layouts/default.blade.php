f3c0e63b68cbfb199904fb0b3e66dcb5d50c3d24
eric <ericarc@gmail.com>
2020-04-20 04:06:11 +0000

    home01

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Weibo App')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
+    
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
